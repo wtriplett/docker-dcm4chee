74c1902574ff720ea94b6ba5bf3fb51daca3caed
avenda <avenda@gmail.com>
2015-12-10 15:46:11 -0500

    update docker file

--- stage/start.bash
+++ stage/start.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -v
 
+sed -i 's/localhost/'"$PACSDB_PORT_3306_TCP_ADDR"'/g' ${DCM_DIR}/server/default/deploy/pacs-mysql-ds.xml
+
 /usr/bin/mysqld_safe &
 /var/local/dcm4chee/dcm4chee-2.18.0-mysql/bin/run.sh
