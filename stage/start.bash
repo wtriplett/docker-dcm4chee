fa315f2e52db13921ff9d7299fc12da3f7343569
Victor Avendano <avenda@gmail.com>
2015-12-11 16:16:28 -0300

    fix path and working dir

--- stage/start.bash
+++ stage/start.bash
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 set -v
 
 sed -i 's/localhost/'"$PACSDB_PORT_3306_TCP_ADDR"'/g' ${DCM_DIR}/server/default/deploy/pacs-mysql-ds.xml
 
+cd ${DCM_DIR}/server/default/conf/
+
 /stage/update_login_config.py
 
 /usr/bin/mysqld_safe &
-/var/local/dcm4chee/dcm4chee-${DCM_ARC_VERSION}-mysql/bin/run.sh
+${DCM_DIR}/bin/run.sh
