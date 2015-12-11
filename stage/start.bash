8ec424ff490ce9f2b12f17d556008d501d72483d
Victor Avendano <avenda@gmail.com>
2015-12-11 19:39:51 -0300

    added arr

--- stage/start.bash
+++ stage/start.bash
@@ -2,14 +2,20 @@
 
 set -v
 
-sed -i 's/localhost/'"$PACSDB_PORT_3306_TCP_ADDR"'/g' ${DCM_DIR}/server/default/deploy/pacs-mysql-ds.xml
+sed -i 's/localhost/'"$PACSDB_PORT_3306_TCP_ADDR"'/g' \
+${DCM_DIR}/server/default/deploy/pacs-mysql-ds.xml
+
+sed -i 's/localhost/'"$ARRDB_PORT_3306_TCP_ADDR"'/g' \
+${DCM_DIR}/server/default/deploy/arr-mysql-ds.xml
 
 cd ${DCM_DIR}/server/default/conf/
 
 /stage/update_login_config.py
 
 echo "Waiting for pacsdb"
-until mysql -h"$PACSDB_PORT_3306_TCP_ADDR" -P"$PACSDB_PORT_3306_TCP_PORT" -uroot -p"$PACSDB_ENV_MYSQL_ROOT_PASSWORD" &> /dev/null
+until \
+mysql -h"$PACSDB_PORT_3306_TCP_ADDR" -P"$PACSDB_PORT_3306_TCP_PORT" \
+-uroot -p"$PACSDB_ENV_MYSQL_ROOT_PASSWORD" &> /dev/null
 do
   printf "."
   sleep 1
