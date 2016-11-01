eafc2c074c7f065aef8ef46fb531111d0c5279f5
William Triplett <wtriplett@ufl.edu>
2016-11-01 05:15:30 -0400

    Update database creation script to also grant access

--- stage/create_dcm4chee_databases.sql
+++ stage/create_dcm4chee_databases.sql
@@ -1,5 +1,6 @@
 create database pacsdb;
-grant all on pacsdb.* to 'pacs'@'localhost' identified by 'pacs';
+grant all on pacsdb.* to 'pacs'@'%' identified by 'pacs';
 
 create database arrdb;
-grant all on arrdb.* to 'arr'@'localhost' identified by 'arr';
+grant all on arrdb.* to 'arr'@'%' identified by 'arr';
+
